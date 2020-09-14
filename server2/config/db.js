1e1539f41899fb650dee9f0ffb4c26e35b71d1a8
heavenlyfist <39553621+heavenlyfist@users.noreply.github.com>
2020-09-14 05:21:54 -0400

    Update db.js

--- server2/config/db.js
+++ server2/config/db.js
@@ -16,13 +16,4 @@ const connectDB = async () => {
   }
 };
 module.exports = connectDB;
-/* Might need these keys below for default.json
-{
-  "mongoURI":"mongodb+srv://jacob:jacob@johncage-wykta.mongodb.net/test?retryWrites=true&w=majority",
-  "jwtSecret":"Cawaw%T<c6.)(tyr}XSz)SefG(7]#4)~"
-} 
-{
-    "mongoURI": "mongodb+srv://yoan:iching433@johncage-wykta.mongodb.net/test?retryWrites=true&w=majority",
-    "jwtSecret": "secretofcage"
-}
-*/
+
