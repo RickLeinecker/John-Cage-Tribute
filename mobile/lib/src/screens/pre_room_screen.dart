b2f792502f6dd78b2524882bea87dd5d49de9bb7
DreamsInCS <yoannierhermida@knights.ucf.edu>
2020-11-20 04:46:48 -0500

    Refactored PreRoomScreen UI

--- mobile/lib/src/screens/pre_room_screen.dart
+++ mobile/lib/src/screens/pre_room_screen.dart
@@ -14,6 +14,7 @@ class PreRoomScreen extends StatelessWidget {
     final bloc = RoomProvider.of(context);
 
     return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
       appBar: AppBar(
         automaticallyImplyLeading: false,
         centerTitle: true,
@@ -38,11 +39,6 @@ class PreRoomScreen extends StatelessWidget {
 
     return Stack(
       children: [
-        SizedBox.expand(
-          child: Container(
-            color: Theme.of(context).primaryColor,
-          ),
-        ),
         Center(
           child: Icon(
             Icons.warning,
@@ -72,69 +68,74 @@ class PreRoomScreen extends StatelessWidget {
             ),
             child: Container(
               height: MediaQuery.of(context).size.height * screenRatio,
-              child: ListView(
-                children: [
-                  Divider(
-                    color: Colors.transparent,
-                    height: 20.0,
+              child: Center(
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Divider(
+                        color: Colors.transparent,
+                        height: 20.0,
+                      ),
+                      Text(
+                        'HALT!',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 75.0,
+                        ),
+                      ),
+                      Divider(
+                        color: Colors.transparent,
+                        height: 35.0,
+                      ),
+                      Text(
+                        'Before you continue, please be warned about this important note.',
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context).textTheme.bodyText1,
+                      ),
+                      Divider(
+                        color: Colors.transparent,
+                        height: 20.0,
+                      ),
+                      Text(
+                        'Use of this application for recording copyrighted material may result in serious legal consequences.',
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context).textTheme.bodyText1,
+                      ),
+                      Divider(
+                        color: Colors.transparent,
+                        height: 20.0,
+                      ),
+                      Text(
+                        'We of John Cage Tribute\'s team are not liable for such behavior.',
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context).textTheme.bodyText1,
+                      ),
+                      Divider(
+                        color: Colors.transparent,
+                        height: 20.0,
+                      ),
+                      Text(
+                        'Creativity is key for this app, but it too has limits. '
+                        'Use its session feature at your own risk.',
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context).textTheme.bodyText1,
+                      ),
+                      Divider(
+                        color: Colors.transparent,
+                        height: 30.0,
+                      ),
+                    ],
                   ),
-                  Text(
-                    'HALT!',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 75.0,
-                    ),
-                  ),
-                  Divider(
-                    color: Colors.transparent,
-                    height: 35.0,
-                  ),
-                  Text(
-                    'Before you continue, please be warned about this important note.',
-                    textAlign: TextAlign.center,
-                    style: Theme.of(context).textTheme.bodyText1,
-                  ),
-                  Divider(
-                    color: Colors.transparent,
-                    height: 20.0,
-                  ),
-                  Text(
-                    'Use of this application for recording copyrighted material may result in serious legal consequences.',
-                    textAlign: TextAlign.center,
-                    style: Theme.of(context).textTheme.bodyText1,
-                  ),
-                  Divider(
-                    color: Colors.transparent,
-                    height: 20.0,
-                  ),
-                  Text(
-                    'We of John Cage Tribute\'s team are not liable for such behavior.',
-                    textAlign: TextAlign.center,
-                    style: Theme.of(context).textTheme.bodyText1,
-                  ),
-                  Divider(
-                    color: Colors.transparent,
-                    height: 20.0,
-                  ),
-                  Text(
-                    'Creativity is key for this app, but it too has limits. '
-                    'Use its session feature at your own risk.',
-                    textAlign: TextAlign.center,
-                    style: Theme.of(context).textTheme.bodyText1,
-                  ),
-                  Divider(
-                    color: Colors.transparent,
-                    height: 30.0,
-                  ),
-                ],
+                ),
               ),
             ),
           ),
         ),
         Align(
-          alignment: FractionalOffset.bottomCenter,
-          heightFactor: 11.0,
+          alignment: FractionalOffset(0.5, 0.95),
           child: RaisedButton(
             onPressed: () => loadRoomScreen(context, bloc, user),
             color: Theme.of(context).textTheme.bodyText2.color,
